0a6b278a96df9d06ca1ff1025027998e5e40129b
Gajus Kuizinas <gk@anuary.com>
2015-08-10 16:00:20 +0100

    Updated tests.

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,17 +1,10 @@
 import _ from 'lodash';
 
-let getUnexpectedReducerOutputError,
-    isDomainMap,
+import Immutable from 'immutable';
+
+let isDomainMap,
     isActionMap,
     iterator;
-
-/**
- * @param {String} key Name of the reducer.
- * @param {Object} action
- */
-getUnexpectedReducerOutputError = (key, action) => {
-    return 'Reducer "' + key + '" returned undefined while handling "' + action.type + '" action.';
-};
 
 /**
  * @param {Object.<string, Object>} map
@@ -33,12 +26,18 @@ isActionMap = (map) => {
  * @param {Object} state
  * @param {Object} action
  * @param {String} action.type
- * @param {Object} reducers
+ * @param {Object} collection
  * @return {Object} state
  */
-iterator = (state, action, reducers) => {
-    _.forEach(reducers, (value, domain) => {
-        // console.log('value, name', value, name, 'isActionMap', isActionMap(value), 'isDomainMap', isDomainMap(value));
+iterator = (state, action, collection) => {
+    if (!Immutable.Iterable.isIterable(state)) {
+        throw new Error('Domain must be an instance of Immutable.Iterable.');
+    }
+
+    // console.log('state', state, 'action', action, 'definition', collection);
+
+    _.forEach(collection, (value, domainName) => {
+        // console.log('value', value, 'domain', domainName, 'isActionMap', isActionMap(value), 'isDomainMap', isDomainMap(value));
 
         if (isActionMap(value)) {
             // console.log('action.type', action.type, 'value[action.type]', typeof value[action.type]);
@@ -46,16 +45,16 @@ iterator = (state, action, reducers) => {
             if (value[action.type]) {
                 let result;
 
-                result = value[action.type](state.get(domain), action);
+                result = value[action.type](state.get(domainName), action);
 
-                if (result === undefined) {
-                    throw new Error(getUnexpectedReducerOutputError(domain, action));
+                if (!Immutable.Iterable.isIterable(result)) {
+                    throw new Error('Reducer must return an instance of Immutable.Iterable. "' + domainName + '" domain "' + action.type + '" action reducer result is "' + typeof result + '".');
                 }
 
-                state = state.set(domain, result);
+                state = state.set(domainName, result);
             }
         } else if (isDomainMap(value)) {
-            state = state.set(domain, iterator(state.get(domain), action, value))
+            state = state.set(domainName, iterator(state.get(domainName), action, value))
         } else {
             throw new Error('Reducer definition object value object all values must correspond to a function (action map) or an object (domain map).');
         }
@@ -68,7 +67,24 @@ iterator = (state, action, reducers) => {
  * @param {Object} reducers
  */
 export default (reducers) => {
+    // _.values(reducers).length is used to ignore empty reducer definition.
+    if (isActionMap(reducers) && _.values(reducers).length) {
+        throw new Error('Reducer definition object must begin with a domain map.');
+    }
+
     return (state, action) => {
+        if (!_.isObject(action)) {
+            throw new Error('Action definition parameter must be an object.');
+        }
+
+        if (!_.isString(action.type)) {
+            throw new Error('Action definition parameter "type" property must be a string.');
+        }
+
+        if (!/^[A-Z\_]+$/.test(action.type)) {
+            throw new Error('Action "type" property value must consist only of uppercase alphabetical characters and underscores.');
+        }
+
         return iterator(state, action, reducers);
     };
 };
--- tests/combineReducers.js
+++ tests/combineReducers.js
@@ -7,35 +7,20 @@ import Immutable from 'immutable';
 import combineReducers from './../src/combineReducers';
 
 describe('combineReducers', () => {
-    /*describe('when reducers parameter is not an object', () => {
+    describe('when reducer is used without an action', () => {
         it('throws an error', () => {
-            expect(() => {
-                combineReducers(null);
-            }).to.throw(Error, 'Reducers definition parameter must be an object.');
-        })
-    });
-    describe('when not all reducers parameter property values are functions', () => {
-        it('throws an error', () => {
-            expect(() => {
-                combineReducers({
-                    foo: 'bar'
-                });
-            }).to.throw(Error, 'All reducers definition object property values must be functions. "foo" property is a "string".');
-        })
-    });
-    describe('when reducers parameter is a valid reducers definition object', () => {
-        describe('when reducer is used without an action', () => {
-            it('throws an error', () => {
-                let reducer;
+            let reducer;
 
-                reducer = combineReducers({});
+            reducer = combineReducers({});
 
-                expect(() => {
-                    reducer({});
-                }).to.throw(Error, 'Action definition parameter must be an object.');
-            });
+            expect(() => {
+                reducer({});
+            }).to.throw(Error, 'Action definition parameter must be an object.');
         });
-        describe('when reducer is used with an action that does not define action.type', () => {
+    });
+
+    describe('when reducer is used with an action', () => {
+        describe('that does not define action.type property', () => {
             it('throws an error', () => {
                 let reducer;
 
@@ -43,10 +28,11 @@ describe('combineReducers', () => {
 
                 expect(() => {
                     reducer({}, {});
-                }).to.throw(Error, 'Action definition parameter type property must be a string.');
+                }).to.throw(Error, 'Action definition parameter "type" property must be a string.');
             });
         });
-        describe('when reducer is used with a state thats not an instance of Immutable.Map', () => {
+
+        describe('that has "type" value which does not consists only of uppercase alphabetical characters and underscores', () => {
             it('throws an error', () => {
                 let reducer;
 
@@ -54,64 +40,108 @@ describe('combineReducers', () => {
 
                 expect(() => {
                     reducer({}, {type: 'foo'});
-                }).to.throw(Error, 'State must be an instance of Immutable.Map.');
-            });
+                }).to.throw(Error, 'Action "type" property value must consist only of uppercase alphabetical characters and underscores.');
+            })
         });
-        describe('when reduced state results in undefined', () => {
-            it('throws an error with the name of the action', () => {
-                let reducer,
-                    state;
+    });
 
-                reducer = combineReducers({
-                    foo: () => {}
-                });
+    describe('when reducer is used with a state (domain) thats not an instance of Immutable.Iterator', () => {
+        it('throws an error', () => {
+            let reducer;
 
-                state = Immutable.Map({});
+            reducer = combineReducers({});
 
-                expect(() => {
-                    reducer(state, {type: 'bar'});
-                }).to.throw(Error, 'Reducer "foo" returned undefined while handling "bar" action.');
-            });
+            expect(() => {
+                reducer({}, {type: 'FOO'});
+            }).to.throw(Error, 'Domain must be an instance of Immutable.Iterable.');
         });
+    });
+
+    describe('when reducer is used with a reducer definition object that does not define domain object', () => {
+        it('throws an error', () => {
+            expect(() => {
+                combineReducers({
+                    FOO: () => {
+
+                    }
+                })
+            }).to.throw(Error, 'Reducer definition object must begin with a domain map.');
+        });
+    });
+
+    describe('when reducer produces a value thats not an instance of Immutable.Iterable', () => {
+        it('throws an error', () => {
+            let reducer,
+                state,
+                action;
+
+            reducer = combineReducers({
+                foos: {
+                    FOO: () => {
+
+                    }
+                }
+            });
+
+            state = Immutable.Map({});
+
+            action = {
+                type: 'FOO'
+            };
+
+            // @todo Should name the full domain namespace, e.g. foo.bar.baz.
+
+            expect(() => {
+                reducer(state, action);
+            }).to.throw(Error, 'Reducer must return an instance of Immutable.Iterable. "foos" domain "FOO" action reducer result is "undefined".');
+        });
+    });
+
+    describe('when reducers parameter is a valid reducers definition object', () => {
         it('produces a new state using the reducer', () => {
             let reducer,
                 state;
 
             reducer = combineReducers({
-                foo: (fooState) => {
-                    return fooState.set('bar', 2);
+                foos: {
+                    FOO: (fooState) => {
+                        return fooState.set('bar', 2);
+                    }
                 }
             });
 
             state = Immutable.fromJS({
-                foo: {
+                foos: {
                     bar: 1
                 }
             });
 
-            state = reducer(state, {type: ''});
+            state = reducer(state, {type: 'FOO'});
 
-            expect(state.get('foo').get('bar')).to.equal(2);
+            expect(state.get('foos').get('bar')).to.equal(2);
         });
+
         it('reducer does not mutate the original state', () => {
             let reducer,
                 state;
 
             reducer = combineReducers({
-                foo: (fooState) => {
-                    return fooState.set('bar', 2);
+                foos: {
+                    FOO: (fooState) => {
+                        return fooState.set('bar', 2);
+                    }
                 }
             });
 
             state = Immutable.fromJS({
-                foo: {
+                foos: {
                     bar: 1
                 }
             });
 
-            reducer(state, {type: ''});
+            reducer(state, {type: 'FOO'});
 
-            expect(state.get('foo').get('bar')).to.equal(1);
+            expect(state.get('foos').get('bar')).to.equal(1);
         });
-    });*/
+    });
 });
