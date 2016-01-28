5a5af3155073a4883f8cfc39a284e7f38d355dc8
Gajus Kuizinas <gk@anuary.com>
2016-01-28 13:36:57 +0000

    Added a benchmark.

---
+++ benchmarks/index.js
@@ -0,0 +1,59 @@
+var Benchmark;
+
+Benchmark = require('benchmark');
+Immutable = require('immutable');
+
+global.require = require;
+
+new Benchmark.Suite()
+        .add('rootReducer iteration', {
+            fn: () => {
+                rootReducer(initialState, {
+                    type: 'TEST'
+                });
+            },
+            setup: () => {
+                var combineReducers,
+                    createCombineReducers,
+                    initialState,
+                    rootReducer;
+
+                initialState = Immutable.fromJS({
+                    foo: {
+                        test: 1
+                    },
+                    bar: {
+                        test: 2
+                    },
+                    baz: {
+                        test: 3
+                    }
+                });
+
+                createCombineReducers = require('./../dist/createCombineReducers');
+                combineReducers = createCombineReducers();
+                // combineReducers = require('redux-immutablejs').combineReducers;
+
+                rootReducer = combineReducers({
+                    foo (state, action) {
+                        return state;
+                    },
+                    bar (state) {
+                        return state;
+                    },
+                    baz (state) {
+                        return state;
+                    }
+                });
+            }
+        })
+        .on('start', (event) => {
+            console.log('starting', event.target.name);
+        })
+        .on('cycle', (event) => {
+            console.log('target', String(event.target));
+        })
+        .on('error', (event) => {
+            console.log('error', String(event.target.error));
+        })
+        .run();
