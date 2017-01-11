c0a2022989b4b8930c89c3abb76383045a3439d0
Gajus Kuizinas <gajus@gajus.com>
2017-01-11 11:13:41 +0200

    chore: remove create-index dependency

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,5 +1,3 @@
-'create index';
-
 export getStateName from './getStateName.js';
 export getUnexpectedInvocationParameterMessage from './getUnexpectedInvocationParameterMessage.js';
 export validateNextState from './validateNextState.js';
