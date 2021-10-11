cb234741030cc2f1ca3847c698a513ce18fd786d
Gajus Kuizinas <gajus@gajus.com>
2021-10-11 16:17:03 -0500

    feat: support immutable v4 (fixes #80) BREAKING CHANGE: drop immutable v3 support

--- src/utilities/getUnexpectedInvocationParameterMessage.js
+++ src/utilities/getUnexpectedInvocationParameterMessage.js
@@ -1,3 +1,4 @@
+
 import Immutable from 'immutable';
 import getStateName from './getStateName';
 
@@ -10,7 +11,7 @@ export default (state: Object, reducers: Object, action: Object) => {
 
   const stateName = getStateName(action);
 
-  if (Immutable.isImmutable ? !Immutable.isImmutable(state) : !Immutable.Iterable.isIterable(state)) {
+  if (Immutable.isImmutable ? !Immutable.isImmutable(state) : !Immutable.isCollection(state)) {
     return 'The ' + stateName + ' is of unexpected type. Expected argument to be an instance of Immutable.Collection or Immutable.Record with the following properties: "' + reducerNames.join('", "') + '".';
   }
 
